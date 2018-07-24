cec177b61de3b3d3406fa0b44a340fbf27cc1ffe
tkrasuski <tkrasuski@gazeta.pl>
2018-07-24 14:48:43 +0200

    urls

--- rejestracja/urls.py
+++ rejestracja/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from  rejestracja import settings
 from django.contrib import admin
 from django.urls import path,  re_path
@@ -29,4 +30,4 @@ urlpatterns = [
     re_path(r'^profile/$', views.profile),
     re_path(r'^school/$', views.school),
    
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
