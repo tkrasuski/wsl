ca7ce18fb36dc00dc53d518327131c1e86e41193
tkrasuski <tkrasuski@gazeta.pl>
2018-07-24 13:09:29 +0200

    statik

--- settings.py
+++ settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR)
 STATIC_URL = '/static/'
-STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS =(STATICFILES_DIRS = (os.path.join(BASE_DIR, '../static'),)
